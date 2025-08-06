09ec03f9d428a0593fa0486e11af215c9434bf2a
Ashish mehta <ashishmehta108work@gmail.com>
2025-08-06 15:34:32 +0530

    updating agaain

--- quiz-backend/src/controllers/auth.controller.js
+++ quiz-backend/src/controllers/auth.controller.js
@@ -74,19 +74,15 @@ export const login = async (req, res) => {
 
     console.log("Tokens generated and saved for user:", user.id);
 
-    res
+    return res
       .cookie("access_token", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-        sameSite: "none",
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000, // 1 day
       })
       .cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
-        path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .json({
