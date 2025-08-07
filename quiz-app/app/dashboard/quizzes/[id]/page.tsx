31d5fb00f28fb114e2c91b7a5b4c8c57e8ffbe0d
Ashish mehta <ashishmehtawork108@gmail.com>
2025-08-07 22:36:37 +0530

    ui enhancement

--- quiz-app/app/dashboard/quizzes/[id]/page.tsx
+++ quiz-app/app/dashboard/quizzes/[id]/page.tsx
@@ -422,8 +422,8 @@ export default function TakeQuizPage({
   return (
     <div className="relative min-h-screen dark:bg-zinc-900 bg-white mt-2">
       <div className=" max-w-2xl container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row md:gap-8 gap-2">
-        <div className="xl:absolute top-20 gap-y-2 gap-2 flex items-center    md:flex-col md:text-2xl  text-lg  left-10">
-          <span className="font-medium">Quiz - {quiz.quiz.title}</span>
+        <div className="xl:absolute top-20 gap-y-2 gap-2 flex items-center    md:flex-col md:text-2xl  text-base  left-10">
+          <span className="font-medium max-w-md mx-auto flex flex-wrap">Quiz - {quiz.quiz.title}</span>
           <span className="flex items-center text-xs gap-2">
             <FileText className="h-4 w-4" />
             {quiz.questions.length} questions
