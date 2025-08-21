1c64fe9c13c8a89fcbe884509120aae6e1f8aa22
Ashish mehta <ashishmehtawork108@gmail.com>
2025-08-21 20:39:07 +0530

    auth issues

--- quiz-backend/src/controllers/auth.controller.js
+++ quiz-backend/src/controllers/auth.controller.js
@@ -32,8 +32,8 @@ export const register = async (req, res) => {
     });
 
   res
-    .cookie("access_token", accessToken, { httpOnly: true, secure: true, sameSite: "none", expires: new Date(Date.now() + 60 * 60 * 24 * 1000), domain: process.env.DOMAIN })
-    .cookie("refresh_token", refreshToken, { httpOnly: true, secure: true, sameSite: "none", expires: new Date(Date.now() + 7 * 60 * 60 * 24 * 1000), domain: process.env.DOMAIN })
+    .cookie("access_token", accessToken, { httpOnly: true, secure: true, sameSite: "none", expires: new Date(Date.now() + 60 * 60 * 24 * 1000) })
+    .cookie("refresh_token", refreshToken, { httpOnly: true, secure: true, sameSite: "none", expires: new Date(Date.now() + 7 * 60 * 60 * 24 * 1000) })
     .json({ user, token: accessToken });
 };
 
@@ -81,15 +81,14 @@ export const login = async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        domain: process.env.DOMAIN,
-        expires: new Date(Date.now() + 60 * 60 * 24 * 1000)
+        expires: new Date(Date.now() + 60 * 60 * 24 * 1000),
+
       })
       .cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: true,
         sameSite: "none",
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        domain: process.env.DOMAIN
       })
       .json({
         user: {
