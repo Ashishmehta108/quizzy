1fd7965b3bce30b2c68a3c6bee97ddc2e4ef7d67
Ashish mehta <ashishmehtawork108@gmail.com>
2025-09-07 22:09:15 +0530

    final version of quizzy

--- quiz-app/components/Landing/AiraAgent.tsx
+++ quiz-app/components/Landing/AiraAgent.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import aira from "@/public/aira.jpg";
-import { Button } from "@/components/ui/button";
-import { InteractiveHoverButton } from "../magicui/interactive-hover-button";
 import Link from "next/link";
+import { InteractiveHoverButton } from "../magicui/interactive-hover-button";
 
 export default function AiraIntro() {
   return (
-    <div className="max-w-5xl mx-auto flex flex-col-reverse lg:flex-row items-center justify-between gap-8  lg:gap-16 h-[700px] py-5 px-4 rounded-2xl overflow-hidden">
-      {/* Text Content */}
-      <div className="flex-1 text-center lg:text-left  text-zinc-900 dark:text-zinc-100">
+    <section className="max-w-5xl mx-auto flex flex-col-reverse lg:flex-row items-center justify-between gap-8 lg:gap-16 h-[800px] py-5 px-4 rounded-2xl overflow-hidden">
+
+      <div className="flex-1 text-center lg:text-left text-zinc-900 dark:text-zinc-100">
         <h2 className="text-4xl sm:text-5xl font-extrabold mb-4 drop-shadow-lg">
           Meet Aira, Your Quiz Helper
         </h2>
@@ -17,21 +18,19 @@ export default function AiraIntro() {
           knowledge from the web or your study database. Get instant guidance
           for quizzes, topics, and learning content!
         </p>
-        <Link href={"/dashboard/chat/ai"}>
-          <InteractiveHoverButton className="">
-            Ask Aira Now
-          </InteractiveHoverButton>
+        <Link href="/dashboard/chat/ai">
+          <InteractiveHoverButton>Ask Aira Now</InteractiveHoverButton>
         </Link>
       </div>
 
-      {/* Image */}
       <div className="flex-1 flex justify-center lg:justify-end">
         <Image
           src={aira}
           alt="Aira agent"
           className="rounded-2xl shadow-2xl w-full max-w-[450px] h-[520px] object-cover transition-transform hover:scale-[1.01]"
+          priority
         />
       </div>
-    </div>
+    </section>
   );
 }
